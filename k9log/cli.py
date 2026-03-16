5b3257658b6a99f67d10dbbe6462b9a482df0746
liuhaotian2024-prog <liuhaotian2024@gmail.com>
2026-03-16 09:20:51 -0400

    chore: release v0.2.7

--- k9log/cli.py
+++ k9log/cli.py
@@ -23,7 +23,7 @@ from rich.table import Table
 console = Console()
 
 @click.group()
-@click.version_option(version='0.2.6')
+@click.version_option(version='0.2.7')
 def main():
     """K9log - Engineering-grade Causal Audit"""
     pass
